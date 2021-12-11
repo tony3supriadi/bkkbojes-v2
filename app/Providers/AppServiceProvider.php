90d3ba3cca1d06654f3c011e8cdf7b83b8a291ac
tony3supriadi <toni@stebasia.com>
2021-12-11 09:39:11 +0700

    fix-issue

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if (config('app.env') === 'production') {
+            $this->app->bind('path.public', function () {
+                return base_path() . '../../public_html';
+            });
+        }
     }
 
     /**
